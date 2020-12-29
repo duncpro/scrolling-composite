d5ffe432d0a25033afd9fba3c6d4ea82bc520546
Duncan Proctor <dbp19a@my.fsu.edu>
2020-12-29 17:38:44 -0500

    add more images

--- projects/scrolling-composite-album-art-example/src/app/app.component.ts
+++ projects/scrolling-composite-album-art-example/src/app/app.component.ts
@@ -30,7 +30,20 @@ export class AppComponent implements OnInit, OnDestroy {
       'Dark_Side_of_the_Moon.png',
       'is_this_it.jpg',
       'torches.jpg',
-      'goth_babe.jpg'
+      'goth_babe.jpg',
+      'lana.jpg',
+      'monkeys.jpg',
+      'stones.jpg',
+      'yellow_sub.jpg',
+      'adele.jpg',
+      'michael.jpg',
+      'taylorswift.png',
+      'youngthegiant.jpg',
+      'lorde.jpg',
+      'miley.png',
+      'hendrix.jpg',
+      'hendrix2.jpg',
+      'skynard.jpg'
     ];
     for (const asset of assets) {
       const result = await fetch('./assets/' + asset, {
